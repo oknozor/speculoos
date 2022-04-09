d9c5e69d20eb1a281f1a72128474c95e067a93d8
Paul Delafosse <paul.delafosse@protonmail.com>
2022-04-09 11:42:55 +0200

    feat: use AsRef trait to accept any kind of string

--- src/string.rs
+++ src/string.rs
@@ -2,57 +2,60 @@ use super::{AssertionFailure, DescriptiveSpec, Spec};
 
 use std::borrow::Borrow;
 
-pub trait StrAssertions {
-    fn starts_with<'r, E: Borrow<&'r str>>(&mut self, expected: E);
-    fn ends_with<'r, E: Borrow<&'r str>>(&mut self, expected: E);
-    fn contains<'r, E: Borrow<&'r str>>(&mut self, expected: E);
-    fn does_not_contain<'r, E: Borrow<&'r str>>(&mut self, expected: E);
+pub trait StrAssertions<T> {
+    fn starts_with<E: AsRef<str>>(&mut self, expected: E);
+    fn ends_with<E: AsRef<str>>(&mut self, expected: E);
+    fn contains<E: AsRef<str>>(&mut self, expected: E);
+    fn does_not_contain<E: AsRef<str>>(&mut self, expected: E);
     fn is_empty(&mut self);
 }
 
-impl<'s> StrAssertions for Spec<'s, &'s str> {
+impl<'s, T> StrAssertions<T> for Spec<'s, T>
+where
+    T: AsRef<str>,
+{
     /// Asserts that the subject `&str` starts with the provided `&str`.
     ///
     /// ```rust
     /// # use speculoos::prelude::*;
-    /// assert_that(&"Hello").starts_with(&"H");
+    /// assert_that(&"Hello").starts_with("H");
     /// ```
-    fn starts_with<'r, E: Borrow<&'r str>>(&mut self, expected: E) {
-        let subject = self.subject;
-        starts_with(self, subject, expected);
+    fn starts_with<E: AsRef<str>>(&mut self, expected: E) {
+        let subject = self.subject.as_ref();
+        starts_with(self, subject, expected.as_ref());
     }
 
     /// Asserts that the subject `&str` ends with the provided `&str`.
     ///
     /// ```rust
     /// # use speculoos::prelude::*;
-    /// assert_that(&"Hello").ends_with(&"o");
+    /// assert_that(&"Hello").ends_with("o");
     /// ```
-    fn ends_with<'r, E: Borrow<&'r str>>(&mut self, expected: E) {
-        let subject = self.subject;
-        ends_with(self, subject, expected);
+    fn ends_with<E: AsRef<str>>(&mut self, expected: E) {
+        let subject = self.subject.as_ref();
+        ends_with(self, subject, expected.as_ref());
     }
 
     /// Asserts that the subject `&str` contains the provided `&str`.
     ///
     /// ```rust
     /// # use speculoos::prelude::*;
-    /// assert_that(&"Hello").contains(&"e");
+    /// assert_that(&"Hello").contains("e");
     /// ```
-    fn contains<'r, E: Borrow<&'r str>>(&mut self, expected: E) {
-        let subject = self.subject;
-        contains(self, subject, expected);
+    fn contains<E: AsRef<str>>(&mut self, expected: E) {
+        let subject = self.subject.as_ref();
+        contains(self, subject, expected.as_ref());
     }
 
     /// Asserts that the subject `&str` contains the provided `&str`.
     ///
     /// ```rust
     /// # use speculoos::prelude::*;
-    /// assert_that(&"Hello").contains(&"e");
+    /// assert_that(&"Hello").contains("e");
     /// ```
-    fn does_not_contain<'r, E: Borrow<&'r str>>(&mut self, expected: E) {
-        let subject = self.subject;
-        does_not_contain(self, subject, expected);
+    fn does_not_contain<E: AsRef<str>>(&mut self, expected: E) {
+        let subject = self.subject.as_ref();
+        does_not_contain(self, subject, expected.as_ref());
     }
 
     /// Asserts that the subject `&str` is empty.
@@ -62,64 +65,7 @@ impl<'s> StrAssertions for Spec<'s, &'s str> {
     /// assert_that(&"").is_empty();
     /// ```
     fn is_empty(&mut self) {
-        let subject = self.subject;
-        is_empty(self, subject);
-    }
-}
-
-impl<'s> StrAssertions for Spec<'s, String> {
-    /// Asserts that the subject `String` starts with the provided `&str`.
-    ///
-    /// ```rust
-    /// # use speculoos::prelude::*;
-    /// assert_that(&"Hello".to_owned()).starts_with(&"H");
-    /// ```
-    fn starts_with<'r, E: Borrow<&'r str>>(&mut self, expected: E) {
-        let subject = &self.subject;
-        starts_with(self, subject, expected);
-    }
-
-    /// Asserts that the subject `String` ends with the provided `&str`.
-    ///
-    /// ```rust
-    /// # use speculoos::prelude::*;
-    /// assert_that(&"Hello".to_owned()).ends_with(&"o");
-    /// ```
-    fn ends_with<'r, E: Borrow<&'r str>>(&mut self, expected: E) {
-        let subject = &self.subject;
-        ends_with(self, subject, expected);
-    }
-
-    /// Asserts that the subject `String` contains the provided `&str`.
-    ///
-    /// ```rust
-    /// # use speculoos::prelude::*;
-    /// assert_that(&"Hello".to_owned()).contains(&"e");
-    /// ```
-    fn contains<'r, E: Borrow<&'r str>>(&mut self, expected: E) {
-        let subject = &self.subject;
-        contains(self, subject, expected);
-    }
-
-    /// Asserts that the subject `String` does not contain the provided `&str`.
-    ///
-    /// ```rust
-    /// # use speculoos::prelude::*;
-    /// assert_that(&"Hello".to_owned()).does_not_contain(&"x");
-    /// ```
-    fn does_not_contain<'r, E: Borrow<&'r str>>(&mut self, expected: E) {
-        let subject = &self.subject;
-        does_not_contain(self, subject, expected);
-    }
-
-    /// Asserts that the subject `String` is empty.
-    ///
-    /// ```rust
-    /// # use speculoos::prelude::*;
-    /// assert_that(&"".to_owned()).is_empty();
-    /// ```
-    fn is_empty(&mut self) {
-        let subject = &self.subject;
+        let subject = self.subject.as_ref();
         is_empty(self, subject);
     }
 }
@@ -195,8 +141,8 @@ fn is_empty<'s, S: DescriptiveSpec<'s>>(spec: &'s S, subject: &str) {
 
 #[cfg(test)]
 mod tests {
-
     use super::super::prelude::*;
+    use std::borrow::Cow;
 
     #[test]
     fn should_allow_multiple_borrow_forms_for_str() {
@@ -204,20 +150,26 @@ mod tests {
         assert_that(&value).starts_with("H");
         assert_that(&value).starts_with(&mut "H");
         assert_that(&value).starts_with(&"H");
+        assert_that(&value).starts_with(Cow::from("H"));
+        assert_that(&value).starts_with("H".to_string());
 
         assert_that(&value).ends_with("o");
         assert_that(&value).ends_with(&mut "o");
         assert_that(&value).ends_with(&"o");
+        assert_that(&value).ends_with(Cow::from("o"));
+        assert_that(&value).ends_with("o".to_string());
 
         assert_that(&value).contains("l");
         assert_that(&value).contains(&mut "l");
         assert_that(&value).contains(&"l");
+        assert_that(&value).contains(Cow::from("l"));
+        assert_that(&value).contains("l".to_string());
     }
 
     #[test]
     fn should_not_panic_if_str_starts_with_value() {
         let value = "Hello";
-        assert_that(&value).starts_with(&"H");
+        assert_that(&value).starts_with("H");
     }
 
     #[test]
@@ -225,32 +177,32 @@ mod tests {
                    \n\t but was: <\"Hello\">")]
     fn should_panic_if_str_does_not_start_with_value() {
         let value = "Hello";
-        assert_that(&value).starts_with(&"A");
+        assert_that(&value).starts_with("A");
     }
 
     #[test]
     fn should_not_panic_if_str_ends_with_value() {
         let value = "Hello";
-        assert_that(&value).ends_with(&"o");
+        assert_that(&value).ends_with("o");
     }
 
     #[test]
     #[should_panic(expected = "\n\texpected: string ending with <\"A\">\n\t but was: <\"Hello\">")]
     fn should_panic_if_str_does_not_end_with_value() {
         let value = "Hello";
-        assert_that(&value).ends_with(&"A");
+        assert_that(&value).ends_with("A");
     }
 
     #[test]
     fn should_not_panic_if_str_contains_value() {
         let value = "Hello";
-        assert_that(&value).contains(&"l");
+        assert_that(&value).contains("l");
     }
 
     #[test]
     fn should_not_panic_if_str_does_not_contains_value() {
         let value = "Hello";
-        assert_that(&value).does_not_contain(&"x");
+        assert_that(&value).does_not_contain("x");
     }
 
     #[test]
@@ -259,14 +211,14 @@ mod tests {
     )]
     fn should_panic_if_str_contains_value() {
         let value = "Hello";
-        assert_that(&value).does_not_contain(&"l");
+        assert_that(&value).does_not_contain("l");
     }
 
     #[test]
     #[should_panic(expected = "\n\texpected: string containing <\"A\">\n\t but was: <\"Hello\">")]
     fn should_panic_if_str_does_not_contain_value() {
         let value = "Hello";
-        assert_that(&value).contains(&"A");
+        assert_that(&value).contains("A");
     }
 
     #[test]
@@ -288,20 +240,26 @@ mod tests {
         assert_that(&value).starts_with("H");
         assert_that(&value).starts_with(&mut "H");
         assert_that(&value).starts_with(&"H");
+        assert_that(&value).starts_with(Cow::from("H"));
+        assert_that(&value).starts_with("H".to_string());
 
         assert_that(&value).ends_with("o");
         assert_that(&value).ends_with(&mut "o");
         assert_that(&value).ends_with(&"o");
+        assert_that(&value).ends_with(Cow::from("o"));
+        assert_that(&value).ends_with("o".to_string());
 
         assert_that(&value).contains("l");
         assert_that(&value).contains(&mut "l");
         assert_that(&value).contains(&"l");
+        assert_that(&value).contains(Cow::from("l"));
+        assert_that(&value).contains("l".to_string());
     }
 
     #[test]
     fn should_not_panic_if_string_starts_with_value() {
         let value = "Hello".to_owned();
-        assert_that(&value).starts_with(&"H");
+        assert_that(&value).starts_with("H");
     }
 
     #[test]
@@ -309,33 +267,33 @@ mod tests {
                    \n\t but was: <\"Hello\">")]
     fn should_panic_if_string_does_not_start_with_value() {
         let value = "Hello".to_owned();
-        assert_that(&value).starts_with(&"A");
+        assert_that(&value).starts_with("A");
     }
 
     #[test]
     fn should_not_panic_if_string_ends_with_value() {
         let value = "Hello".to_owned();
-        assert_that(&value).ends_with(&"o");
+        assert_that(&value).ends_with("o");
     }
 
     #[test]
     #[should_panic(expected = "\n\texpected: string ending with <\"A\">\n\t but was: <\"Hello\">")]
     fn should_panic_if_string_does_not_end_with_value() {
         let value = "Hello".to_owned();
-        assert_that(&value).ends_with(&"A");
+        assert_that(&value).ends_with("A");
     }
 
     #[test]
     fn should_not_panic_if_string_contains_value() {
         let value = "Hello".to_owned();
-        assert_that(&value).contains(&"l");
+        assert_that(&value).contains("l");
     }
 
     #[test]
     #[should_panic(expected = "\n\texpected: string containing <\"A\">\n\t but was: <\"Hello\">")]
     fn should_panic_if_string_does_not_contain_value() {
         let value = "Hello".to_owned();
-        assert_that(&value).contains(&"A");
+        assert_that(&value).contains("A");
     }
 
     #[test]
